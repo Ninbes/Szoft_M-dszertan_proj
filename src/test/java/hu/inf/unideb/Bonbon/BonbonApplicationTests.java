519d364760f25e903efec97dc9a9b804044fbe7c
Papp Bence Máté <bencemate@mailbox.unideb.hu>
2024-09-26 18:27:02 +0200

    Initial spring proj

---
+++ src/test/java/hu/inf/unideb/Bonbon/BonbonApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb.Bonbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonbonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
